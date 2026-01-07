153a0dc8adc137316755e5743b186e235f71869c
Vi Trung <vitrung06022004@gmail.com>
2026-01-07 22:58:50 +0700

    setup weather-dashboard

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-5xl font-bold text-blue-500 bg-white p-10 rounded-3xl shadow-2xl">
+        Tailwind đã chạy! 🚀
+      </h1>
+    </div>
+  )
+}
+export default App
